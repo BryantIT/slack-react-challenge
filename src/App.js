6e597c298b05af3307f6abc4fd5808bb88062c10
Bryant Richards <dragonseyeimages@gmail.com>
2021-03-02 11:13:16 -0600

    setup styles for sidebar

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 // Components
-import Chat from './components/chats/Chat'
-import Login from './components/login/Login'
-import Header from './components/header/Header'
+import Chat from "./components/chats/Chat";
+import Login from "./components/login/Login";
+import Header from "./components/header/Header";
+import Sidebar from "./components/sidebar/sidebar";
 // Packages
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import styled from 'styled-components'
-import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import styled from "styled-components";
+import "./App.css";
 
 function App() {
+  const Main = styled.div`
+    display: grid;
+    grid-template-columns: 260px auto
+  `
   const Container = styled.div`
-
+    width: 100%;
+    height: 100vh;
+    display: grid;
+    grid-template-rows: 38px auto;
   `
 
   return (
     <div className="App">
-    <Router>
-      <Container>
-        <Header />
-        <Switch>
-          <Route path="/room">
-            <Chat />
-          </Route>
-          <Route path="/">
-            <Login />
-          </Route>
-        </Switch>
-      </Container>
-    </Router>
-  </div>)
+      <Router>
+        <Container>
+          <Header />
+          <Main>
+            <Sidebar />
+            <Switch>
+              <Route path="/room">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Login />
+              </Route>
+            </Switch>
+          </Main>
+        </Container>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
